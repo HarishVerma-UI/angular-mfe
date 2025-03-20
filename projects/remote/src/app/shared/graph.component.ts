a1dfa28a8b596dc0951ceead4c37c28d194c1981
Harish Kumar Verma <VERM396533@exactsoftware.com>
2025-03-20 09:25:58 +0530

    Fixed graph load

--- projects/remote/src/app/shared/graph.component.ts
+++ projects/remote/src/app/shared/graph.component.ts
@@ -16,16 +16,32 @@ export class GraphComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (this.chartInstance) {
       this.chartInstance.destroy();
+      this.chartInstance = undefined;
     }
-    const ctx = (
-      document.getElementById(this.chartId) as HTMLCanvasElement
-    ).getContext('2d');
+
+    if (!this.chartId) {
+      console.error('Chart ID is undefined. Please provide a valid chart ID.');
+      return;
+    }
+
+    const canvasElement = document.getElementById(
+      this.chartId
+    ) as HTMLCanvasElement;
+    if (!canvasElement) {
+      return;
+    }
+
+    const ctx = canvasElement.getContext('2d');
     if (ctx) {
       this.chartInstance = new Chart(ctx, {
         type: this.chartType,
         data: this.chartData,
         options: this.chartOptions,
       });
+    } else {
+      console.error(
+        `Failed to get 2D context for canvas with id '${this.chartId}'.`
+      );
     }
   }
 }
